ccc604f4f2c02aacd49ad0f38c27a1fc0c4cb07e
Tech77 <101491745+chrlschwb@users.noreply.github.com>
2022-09-14 07:04:00 -0800

    140

--- src/config.js
+++ src/config.js
@@ -1,6 +1,15 @@
 
 export const NODE_URL = "http://localhost:5000"; 
  
-export const APP_ID = "glKSCAWH3oGNrzbG581lYX25mXu7qh84qCH4nd7d";
+export const SERVER_URL = "https://4stgo3byiouu.usemoralis.com:2053/server";
 
-export const SERVER_URL = "https://hbnfhwab61xo.usemoralis.com:2053/server"; 
+export const APP_ID = "WM7HbKDQpxeyi1Nfe4gZRwmXmOukv4l6FJPBza81";
+
+
+// export const APP_ID = "glKSCAWH3oGNrzbG581lYX25mXu7qh84qCH4nd7d";
+
+// export const SERVER_URL = "https://hbnfhwab61xo.usemoralis.com:2053/server"; 
+
+// export const APP_ID = "sPjCqOdg8qBGXoGtHb9abxSyPVdzygwleEqCOHdA";
+
+// export const SERVER_URL = "https://mtiagbr9hibl.usemoralis.com:2053/server"; 
